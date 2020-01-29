b3d02c5f34fc2a57be9dc02f8aa249163a9c930f
sakdram <60438111+sakdram@users.noreply.github.com>
2020-01-29 20:33:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let Vid1 = ["video/1.mp4","video/2.mp4","video/3.mp4"];
+let Vid2 = ["video/4.mp4","video/5.mp4","video/6.mp4"];
+let Vid3 = ["video/7.mp4","video/8.mp4","video/9.mp4"];
+let Channels = [Vid1,Vid2,Vid3];
+let count = 0;
+let channel = 0;
+
+
+function nextChannel (){
+    channel = channel + 1;
+    count = -1;
+    if (channel===Channels.length){
+        channel = 0;
+    }
+    nex();
+}
+function prevChannel() {
+    if (channel === count){channel = 3;}
+    channel = channel - 1;
+    count = -1;
+    nex();
+}
+
+function nex() {
+    count = count + 1;
+    $("#active").attr("src", Channels[channel][count]);
+    if (count===Channels[channel].length - 1){
+        count = -1;
+    }
+}
